3a33dbb472793662f27269c4e7810065738cf569
Spencer.HUANG <spencer.huang@nio.com>
2022-03-25 10:18:30 +0800

    调整包结构后，显式地指定@Component的扫描范围，防止扫描不到controller。

--- src/main/java/com/spring/puzzle/class1/example1/application/Application.java
+++ src/main/java/com/spring/puzzle/class1/example1/application/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan("com.spring.puzzle.class1.example1.controller")
+@ComponentScan("com.spring.puzzle.class1.example1.controller")
 public class Application {
 
     public static void main(String[] args) {
